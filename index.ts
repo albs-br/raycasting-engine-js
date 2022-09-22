34075fd8c5f1f399d10c253e292ced1a74862fbb
albs-br <albs_br@yahoo.com.br>
2022-09-22 17:02:35 -0300

    v.0.8.0

--- index.ts
+++ index.ts
@@ -10,13 +10,12 @@ let objCanvasScr = document.getElementById('canvasScr');
 let cs = objCanvasScr.getContext('2d');
 
 let p = new Player();
-p.c = c;
 // p.X = 100;
 // p.Y = 20;
-// p.Angle = 60;
+// p.setAngle(60);
 p.X = 187;
 p.Y = 212;
-p.Angle = 164;
+p.setAngle(164);
 
 // const map: number[] = [
 //   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -56,8 +55,7 @@ const map: number[] = [
   [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-// Game loop
-window.setInterval(function () {
+const gameLoop = function () {
   ReadInput();
 
   DrawMap();
@@ -67,9 +65,11 @@ window.setInterval(function () {
   //let s = `{p.X}, {p.Y}, {p.Angle}`;
   let s2 = 'x: ' + p.X + ', y: ' + p.Y + ', a: ' + p.Angle;
   document.getElementById('debuginfo').innerHTML = s2;
+};
 
-  p.Draw();
-}, 33); // 30fps
+// Game loop
+window.setInterval(gameLoop, 33); // 30fps
+//window.setTimeout(gameLoop, 100); // debug
 
 // Handle keyboard input
 let key = [];
@@ -163,18 +163,63 @@ function DrawMap() {
       c.closePath();
     }
   }
+
+  DrawPlayerOnMap();
 }
 
+function DrawPlayerOnMap() {
+  const ARROW_SIZE = 16;
+
+  // center of field of view vector
+  let angle = p.getAngle();
+  c.beginPath();
+  c.strokeStyle = 'red';
+  c.moveTo(p.X, p.Y);
+  c.lineTo(
+    p.X + ARROW_SIZE * Math.cos((angle * Math.PI) / 180),
+    p.Y + ARROW_SIZE * Math.sin((angle * Math.PI) / 180)
+  );
+  c.stroke();
+  c.closePath();
+
+  // start of field of view vector
+  angle = p.getAngle() - FOV / 2;
+  c.beginPath();
+  c.strokeStyle = 'black';
+  c.moveTo(p.X, p.Y);
+  c.lineTo(
+    p.X + ARROW_SIZE * 2 * Math.cos((angle * Math.PI) / 180),
+    p.Y + ARROW_SIZE * 2 * Math.sin((angle * Math.PI) / 180)
+  );
+  c.stroke();
+  c.closePath();
+
+  // end of field of view vector
+  angle = p.getAngle() + FOV / 2;
+  c.beginPath();
+  c.strokeStyle = 'black';
+  c.moveTo(p.X, p.Y);
+  c.lineTo(
+    p.X + ARROW_SIZE * 2 * Math.cos((angle * Math.PI) / 180),
+    p.Y + ARROW_SIZE * 2 * Math.sin((angle * Math.PI) / 180)
+  );
+  c.stroke();
+  c.closePath();
+
+  //c.strokeRect(this.X, this.Y, 100 * Math.sin((this.Angle * Math.PI) / 180), 100);
+  c.strokeRect(p.X, p.Y, 1, 1);
+}
+
+const FOV = 60; // field of view in degrees
+
+const SCREEN_COLS = 256;
+//const SCREEN_COLS = 64;
+
+const RAY_STEP = 256 / SCREEN_COLS;
+const COLUMN_WIDTH = 256 / SCREEN_COLS;
+const MAX_DISTANCE = 256;
+
 function DrawScreen() {
-  const FOV = 60; // field of view in degrees
-
-  const SCREEN_COLS = 256;
-  //const SCREEN_COLS = 64;
-
-  const RAY_STEP = 256 / SCREEN_COLS;
-  const COLUMN_WIDTH = 256 / SCREEN_COLS;
-  const MAX_DISTANCE = 256;
-
   // -------------------------------------------
 
   //TODO: this should be done once at game init, not in game loop...
@@ -186,26 +231,48 @@ function DrawScreen() {
   // convert to radians (Radians = Degrees × π / 180)
   let d = SCREEN_COLS / 2 / Math.tan(((FOV / 2) * Math.PI) / 180); // fixed value (distance from player to viewplane, at forward direction)
 
-  //for (let i = SCREEN_COLS / 2; i >= 1; i--) {
-  for (let i = 1; i <= SCREEN_COLS / 2; i++) {
+  // debug:
+  //console.log(d);
+  // d = adj = 221.7
+  // oposite = 128 (half of SCREEN_COLS)
+  // tg(a) = oposite / adj = 128 / 221.7 = 0.57
+  // tg(30) = 0.57
+
+  for (let i = SCREEN_COLS / 2; i >= 1; i--) {
+    //for (let i = 1; i <= SCREEN_COLS / 2; i++) {
     let angleInRadians = Math.atan((COLUMN_WIDTH * i) / d);
+
+    //console.log(angleInRadians);
 
     // convert to degrees (Degrees = Radians × 180 / π)
     let angleInDegrees = (angleInRadians * 180) / Math.PI;
 
     // store angles, in relation to player's forward direction (which is perpendicular to the view plane)
     tempAngles.push(angleInDegrees);
+
+    //console.log(angleInDegrees);
   }
+
+  //console.log(tempAngles.length);
+
   // store the angle, in relation to player's start of FOV
-  for (let i = 0; i < SCREEN_COLS / 2; i++) {
-    fovAngles.push(tempAngles[i] - tempAngles[0]);
+  fovAngles.push(0);
+  for (let i = 0; i < SCREEN_COLS / 2 - 1; i++) {
+    let t = tempAngles[i] - tempAngles[i + 1];
+    fovAngles.push(t);
+
+    //console.log(t);
   }
+
   // compute the other half of angles FOV (symetrical)
   let j = SCREEN_COLS / 2 - 1;
-  for (let i = 0; i < SCREEN_COLS; i++) {
-    fovAngles.push(-fovAngles[j]);
+  for (let i = 0; i < SCREEN_COLS / 2; i++) {
+    fovAngles.push(fovAngles[j]);
     j--;
   }
+
+  //console.log(fovAngles.length);
+  //console.log(fovAngles);
 
   // ------------------------------------------------
 
@@ -225,27 +292,21 @@ function DrawScreen() {
   cs.fillStyle = grd;
   cs.fillRect(0, 0, 256, 96);
 
-  // set initial angle
+  // set initial to start of FOV
   let angle = p.getAngle() - FOV / 2;
 
   // for each screen column
   for (let col = 0; col < SCREEN_COLS; col++) {
+    //angle += fovAngles[col];
+
     // cast ray from player until find a wall
     let wallFound: boolean = false;
     let distance = 0;
     let rayX = p.X;
     let rayY = p.Y;
 
-    // step from -0.5 to +0.5 in equal increments
-    //let progress = col / SCREEN_COLS - 0.5;
-
-    //angle += progress;
-
     let stepX = RAY_STEP * Math.cos((angle * Math.PI) / 180);
     let stepY = RAY_STEP * Math.sin((angle * Math.PI) / 180);
-
-    // stepX += progress;
-    // stepY += progress;
 
     while (!wallFound && distance < MAX_DISTANCE) {
       rayX += stepX;
@@ -271,6 +332,7 @@ function DrawScreen() {
 
     // this is supposed to fix the "fisheye effect"
     let z = distance * Math.cos(((p.Angle - angle) * Math.PI) / 180);
+    //let z = distance;
 
     // test
     // if (z > 256) z = 256;
